15ec64a38558506d7a177bc7d044d2319657fa8b
Jubibani <strawberryloli3@gmail.com>
2024-10-10 02:33:29 +0800

    added activity 3

---
+++ activity3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplication Table</title>
+</head>
+<body>
+<body>
+    <?php
+        for ($i = 1; $i < 11; $i++) {
+            $result = 7 * $i;
+            
+            $printCalculation = "7 x " . $i ." = " .$result;
+            echo $printCalculation . "<br>"; // no more "\" because we no no in terminal anymore.
+        }
+    ?>
+</body>
+
+</body>
+</html>
