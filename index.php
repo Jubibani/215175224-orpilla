34fbbf5108dd1afdde8defa1b07948b414e8b087
Jubibani <strawberryloli3@gmail.com>
2024-11-14 04:59:37 +0800

    change html to php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercises Index</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="layout">
+        <div class="sidebar">
+            <h1>PHP Exercises</h1>
+            <div class="author">By Christopher Jiovanni Orpilla</div>
+        </div>
+        <div class="container">
+            <ul class="exercise-list">
+                <li><a href="php/arrayIteration.php">Array Iteration</a></li>
+                <li><a href="php/factorialCalculator.php">Factorial Calculator</a></li>
+                <li><a href="php/fibonacciSequence.php">Fibonacci Sequence</a></li>
+                <li><a href="php/fizzBuzz.php">FizzBuzz</a></li>
+                <li><a href="php/keyvaluePairs.php">Key Value Pairs</a></li>
+                <li><a href="php/loopControl.php">Loop Control</a></li>
+                <li><a href="php/multiplicationTable.php">Multiplication Table</a></li>
+                <li><a href="php/numberCounter.php">Number Counter</a></li>
+                <li><a href="php/passwordValidator.php">Password Validator</a></li>
+                <li><a href="php/primenumberChecker.php">Prime Number Checker</a></li>
+                <li><a href="php/reverseString.php">Reverse String</a></li>
+                <li><a href="php/sumOfNumbers.php">Sum of Numbers</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
